6637581e521f955887cc85b24cdaa353ca761f39
yerrarao1221 <87896237+yerrarao1221@users.noreply.github.com>
2022-03-28 15:05:27 +0530

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("Buddy Works with Heroku", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("Purushotham Buddy Works with Heroku!...", new String( out.toByteArray(), "UTF-8"));
     }
 }
